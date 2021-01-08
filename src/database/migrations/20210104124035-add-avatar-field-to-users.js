b48b9b85f200eb6de55ab8aa46f51d0c68227268
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2021-01-08 15:07:52 -0300

    index providers

---
+++ src/database/migrations/20210104124035-add-avatar-field-to-users.js
@@ -0,0 +1,16 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) =>
+    queryInterface.addColumn('users', 'avatar_id', {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'files',
+        key: 'id',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL',
+      allowNull: true,
+    }),
+
+  down: async (queryInterface) =>
+    queryInterface.removeColumn('users', 'avatar_id'),
+};
